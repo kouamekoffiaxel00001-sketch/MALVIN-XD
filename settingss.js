c2e61f5878f1bd337e69f8da37df0114a759d6b7
kouamekoffiaxel00001-sketch <kouamekoffiaxel00001@gmail.com>
2025-08-20 19:34:08 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,9 +1,9 @@
 
 module.exports = {
-  SESSION_ID: ""
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~pylyybSQ#NvfC02Cr4LTvORLDDkm7x1vn85RlsPh6FZZm3UNPCfA"
+  OWNER_NUMBER: "2250715405077", // put your phone number here
   
-  PREFIX: "*", // prefix (e.g., ., /, !, *)
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
